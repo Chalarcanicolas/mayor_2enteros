eec50607d2e9fa616c0bc9c08b3b1bf3ed093cf9
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-02-23 16:08:20 -0500

    Se modifica el programa.py

--- mayor_2enteros.py
+++ mayor_2enteros.py
@@ -6,12 +6,14 @@ x = int (input("Digite el valor de x: "))
 y = int (input("digite el valor de y: "))
 
 # processing
-if x > y:
-    mayor = x
-else: 
-    mayor = y
-    # output 
-print("El mayor entre " + str(x) +  " y "  + str(y) +  " es " +  str(mayor))
 if x == y:
     #output
     print(" Los numeros son iguales...")
+else:
+    if x > y:
+        mayor = x
+    else: 
+        mayor = y
+        # output 
+        print("El mayor entre " + str(x) +  " y "  + str(y) +  " es " +  str(mayor))
+
